9078c5b590a3cf41f9e6b694fccfb9950f7b12ff
jshaw-7 <98419655+jshaw-7@users.noreply.github.com>
2022-10-17 14:38:02 -0400

    initial commit

---
+++ play.py
@@ -0,0 +1,128 @@
+import numpy as np 
+import pandas as pd 
+import random
+
+import string
+import re
+from collections import Counter
+from tqdm import tqdm
+
+import colorama
+from colorama import Fore
+
+with open('wordle-allowed-guesses.txt', 'r') as f:
+    allowed = f.read().splitlines()
+with open('wordle-answers-alphabetical.txt','r') as f:
+    answers = f.read().splitlines()
+allowed += answers
+
+starting_words = ['saint', 'saner', 'shied', 'crier', 'cried', 'sleek', 'sleep', 'sleet', 
+                 'slant', 'slang', 'brief', 'slash', 'siren', 'slain', 'sling', 'slink', 
+                 'surer', 'sooty', 'sooth', 'slack']
+
+def get_guess(words, answers):
+    guess = ''
+    correct = 0
+    while correct == 0:
+        guess = input('input a 5-letter word: ')
+        if len(guess) != 5:
+            print('word must be 5 letters.')
+        elif (guess not in words):
+            print('please input a real word.')
+        else:
+            correct = 7
+    return guess
+    
+def check_word(guess, status, choice):
+    for i in range(5):
+        for x in range(5):
+            if guess[i] == choice[x] and x == i:
+                status[i] = 2
+                break
+            elif guess[i] == choice[x]:
+                status[i] = 1
+                break
+    return sum(status)
+
+def print_word(guess, status):
+    for i in range(5):
+        if status[i] == 2:
+            print(Fore.GREEN + guess[i])
+        elif status[i] == 1:
+            print(Fore.YELLOW + guess[i])
+        else:
+            print(Fore.RED + guess[i])
+    print()
+
+def play(choice):
+    attempts = 0
+    guesses = 6
+    won = False
+    
+    print(Fore.GREEN + "this is WORDLE!")
+    print("you have 6 tries to guess the 5-letter word i'm thinking of ... ")
+
+    for i in range(6):
+        attempts +=1
+        guess = get_guess(allowed, answers)
+        status = [0, 0, 0, 0, 0]
+        score = check_word(guess, status, choice)
+        print('guess ' + str(i+1) + ": ")
+        print_word(guess, status)
+        if (score >= 10) or (choice == guess):
+            won = True
+            break
+    if won:
+        print(Fore.GREEN + "you won! the correct word was " + choice)
+    else:
+        print(Fore.RED + "you lost. the correct word was " + choice)
+    return attempts, won
+
+def solve(choice):
+    possible = allowed
+    guesses = 0
+    while True:
+        guesses +=1
+        guess = make_guess(possible, guesses)
+        print(Fore.RESET + "bot guess " + str(guesses) + ": "+ guess.upper())
+        result = get_result(guess, choice)
+        if result == "!!!!!":
+            break
+        possible = update_possible(possible, guess, result)
+    return guesses
+
+def get_result(guess, answer):
+    result = ""
+    for pos, ch_guess, ch_answer in zip(range(5), guess, answer):
+        if ch_guess == ch_answer:
+            result += "!"
+        elif ch_guess not in answer:
+            result += "_"
+        else:
+            result += "?"
+    return result
+  
+def update_possible(possible, guess, result):
+    return [word for word in possible if get_result(guess, word) == result]
+
+def make_guess(possible, guess):
+    for i in range(20):
+        if starting_words[i] in possible:
+            return starting_words[i]
+    return random.choice(possible)
+
+
+choice = answers[random.randint(0, 6)]
+print(choice)
+
+player, won = play(choice)
+bot = solve(choice)
+
+if not won:
+    print(Fore.RED + ('you did not get the word "' + choice +'", and it took the ai ' + str(bot) + ' guesses. better luck next time!'))
+elif (player < bot) and (player == 1):
+    print(Fore.GREEN + ('the word "' + choice +'" took you ' + str(player) + ' guess and took the ai ' + str(bot) + ' guesses. good job!'))
+elif(player <= bot):
+    print(Fore.GREEN + ('the word "' + choice +'" took you ' + str(player) + ' guesses and took the ai ' + str(bot) + ' guesses. good job!'))
+else:
+    print(Fore.RED + ('the word "' + choice +'" took you ' + str(player) + ' guesses and took the ai ' + str(bot) + ' guesses. better luck next time!'))
---
+++ starting.py
@@ -0,0 +1,94 @@
+import numpy as np 
+import pandas as pd 
+
+import os
+
+from pyod.models.copod import COPOD
+import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+import string
+import re
+from collections import Counter
+import colorama
+from colorama import Fore
+
+
+with open('wordle-allowed-guesses.txt', 'r') as f:
+    allowed = f.read().splitlines()
+with open('wordle-answers-alphabetical.txt','r') as f:
+    answers = f.read().splitlines()
+allowed += answers
+
+letters = []
+for word in answers:
+    for letter in word:
+        letters.append(letter)
+
+count = Counter(letters)
+
+answers_letter_frequency = pd.DataFrame.from_dict(count, orient='index').sort_values(0, ascending = False)
+answers_letter_frequency.rename(columns = {0:'answer letter frequency',}, inplace = True)
+answers_letter_frequency.plot(kind='bar',figsize=(10,3))
+
+
+letters = []
+for word in allowed:
+    for letter in word:
+        letters.append(letter)
+
+count = Counter(letters)
+
+allowed_letter_frequency = pd.DataFrame.from_dict(count, orient='index').sort_values(0, ascending = False)
+allowed_letter_frequency.rename(columns = {0:'allowed letter frequency',}, inplace = True)
+allowed_letter_frequency.plot(kind='bar',figsize=(10,3))
+
+letters = ['a1', 'a2', 'a3', 'a4', 'a5', 'b1', 'b2', 'b3', 'b4', 'b5', 'c1', 'c2', 'c3', 'c4', 'c5', 
+          'd1', 'd2', 'd3', 'd4', 'd5', 'e1', 'e2', 'e3', 'e4', 'e5', 'f1', 'f2', 'f3', 'f4', 'f5', 
+           'g1', 'g2', 'g3', 'g4', 'g5', 'h1', 'h2', 'h3', 'h4', 'h5', 'i1', 'i2', 'i3', 'i4', 'i5',
+           'j1', 'j2', 'j3', 'j4', 'j5', 'k1', 'k2', 'k3', 'k4', 'k5', 'l1', 'l2', 'l3', 'l4', 'l5', 
+           'm1', 'm2', 'm3', 'm4', 'm5', 'n1', 'n2', 'n3', 'n4', 'n5', 'o1', 'o2', 'o3', 'o4', 'o5', 
+           'p1', 'p2', 'p3', 'p4', 'p5', 'q1', 'q2', 'q3', 'q4', 'q5', 'r1', 'r2', 'r3', 'r4', 'r5', 
+           's1', 's2', 's3', 's4', 's5', 't1', 't2', 't3', 't4', 't5', 'u1', 'u2', 'u3', 'u4', 'u5', 
+           'v1', 'v2', 'v3', 'v4', 'v5', 'w1', 'w2', 'w3', 'w4', 'w5', 'x1', 'x2', 'x3', 'x4', 'x5',
+           'y1', 'y2', 'y3', 'y4', 'y5', 'z1', 'z2', 'z3', 'z4', 'z5']
+
+positions = []
+for word in allowed:
+    tmp = []
+    for i in range(130):
+        tmp.append(0)
+    for i in range(4):
+        letter = ord(word[i]) - 97
+        tmp[(letter*5) + i] = 1
+    positions.append(tmp)
+allowed_letter_positions = pd.DataFrame(positions, index = [allowed], columns = letters)
+
+positions = []
+for word in answers:
+    tmp = []
+    for i in range(130):
+        tmp.append(0)
+    for i in range(4):
+        letter = ord(word[i]) - 97
+        tmp[(letter*5) + i] = 1
+    positions.append(tmp)
+answers_letter_positions = pd.DataFrame(positions, index = [answers], columns = letters)
+
+answers_letter_positions.head()
+
+answers_letter_positions['total_rank'] = 0
+
+copod_model = COPOD()
+copod_model.fit(answers_letter_positions)
+
+answers_letter_positions['score'] = copod_model.decision_scores_
+answers_letter_positions.sort_values('score',inplace=True)
+answers_letter_positions['rank'] = range(1,len(answers_letter_positions)+1)
+answers_letter_positions['total_rank'] += answers_letter_positions['rank']
+
+answers_letter_positions.sort_values('total_rank',inplace=True)
+answers_letter_positions['rank'] = range(1,len(answers_letter_positions)+1)
+letters.append('total_rank')
+answers_letter_positions.drop(columns = letters, inplace = True)
+answers_letter_positions.head(20)
